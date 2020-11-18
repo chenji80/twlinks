df6ee782dae7ad52bb1174ba1bdaae7f615d169c
陈吉 <wx_c84cb20815eb4f44ad0f0595bf576e9b@git.code.tencent.com>
2020-11-18 13:44:45 +0800

    .

--- globalConfig.js
+++ globalConfig.js
@@ -1,9 +1,9 @@
 // env
-window.API_URL = "http://192.168.9.232"
-window.API_URL_WS="ws://192.168.9.232"
+// window.API_URL = "http://192.168.9.232"
+// window.API_URL_WS="ws://192.168.9.232"
 
-window.API_URL = "http://180.169.15.121"
-window.API_URL_WS="ws://180.169.15.121"
+// window.API_URL = "http://180.169.15.121"
+// window.API_URL_WS="ws://180.169.15.121"
 
 // window.API_URL = "http://192.168.3.237"
 // window.API_URL_WS = "ws://192.168.3.237"
@@ -11,8 +11,9 @@ window.API_URL_WS="ws://180.169.15.121"
 // window.API_URL = "http://192.168.9.50:8844"
 // window.API_URL_WS = "ws://192.168.9.50:8844"
 
-// window.API_URL = "http://32.0.15.62"
-// window.API_URL_WS="ws://32.0.15.62"
+window.API_URL = "http://32.0.15.62"
+window.API_URL_WS="ws://32.0.15.62"
+
 
 //门户设备发起流程对接地址
 // "http://dcp.shmh.gov.cn/desktop/#/workdesk/ApplyDataResources?model=add&type=1&resourseType=IOT"
